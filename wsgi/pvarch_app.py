dda9eb8fc80ede4b657c02865cc97888abb2e9d5
Matt Newville <newville@cars.uchicago.edu>
2016-01-25 20:36:30 -0600

    updates for plot()

--- wsgi/pvarch_app.py
+++ wsgi/pvarch_app.py
@@ -5,11 +5,15 @@ from flask import (Flask, request, session, redirect, url_for,
                    abort, render_template, flash, Response)
 
 from werkzeug import secure_filename
+from epics import PV
 
 from EpicsArchiver import config
 from EpicsArchiver.SimpleDB import Connection
 from WebStatus import WebStatus
 from PlotViewer import  PlotViewer
+
+from archive import ArchiveMaster
+
 from make_plot import make_plot
 
 from secret import secret_key
@@ -28,19 +32,24 @@ app.config['SESSION_TYPE'] = 'memcached'
 dbconn = Connection(dbname=config.master_db, user=config.dbuser,
                     passwd=config.dbpass, host=config.dbhost)
 
+arch = ArchiveMaster(user=config.dbuser, password=config.dbpass)
+
+
 @app.route('/')
 def index():
     return redirect(url_for('show', page='General'))
 
+
+@app.route('/help')
+def help():
+    return render_template('help.html')    
+
 @app.route('/show/<page>')
 def show(page=None):
-    # cache = session.get('user_cache', None)
-    # dbconn = session.get('user_dbconn', None)
-
+    """
+    Could be translated to static pages ?
+    """
     p = WebStatus(cache=None, dbconn=dbconn)
-
-    # session['user_cache'] = p.cache
-    # session['user_dbconn'] = p.dbconn
     page = str(page)
     
     p.begin_page(page, pagelist, refresh=60)
@@ -62,32 +71,77 @@ def show(page=None):
 
 @app.route('/plot/<pv>')
 @app.route('/plot/<pv>/<pv2>')
-@app.route('/plot/<pv>/<pv2>/<time_ago>')
-@app.route('/plot/<pv>/<pv2>/<time_ago>/<date1>')
-@app.route('/plot/<pv>/<pv2>/<time_ago>/<date1>/<date2>')
-def plot(pv=None, pv2=None, time_ago=None, date1=None, date2=None):
-    opts = {'pv': pv,
-            'pv2': pv2,
-            'time_ago': time_ago,
-            'date1': date1, 'date2': date2}
+@app.route('/plot/<pv>/<pv2>/<timevar>')
+@app.route('/plot/<pv>/<pv2>/<timevar>/<date1>')
+@app.route('/plot/<pv>/<pv2>/<timevar>/<date1>/<date2>')
+def plot(pv=None, pv2=None, timevar=None, date1=None, date2=None):
+
+    if pv2     in ('', None, 'None'): pv2 = None
+    if timevar in ('', None, 'None'): timevar = 'time_ago'
+    if date1   in ('', None, 'None'): date1 = '1_days'
+    if date2   in ('', None, 'None'): date2 = None
+
+    if timevar == 'time_ago':
+        tval, tunit  = date1.split('_')
+    else:
+        tval, tunit = '1', None
+
+    messages = []
+    ts, dat, enums, ylabel, ylog = None, None, None, None, False
+    try:
+        related = arch.related_pvs(pv)
+        pvinfo  = arch.get_pvinfo(pv)
+        desc    = pvinfo['desc']
+        dtype   = pvinfo['type']
+        if dtype == 'enum':
+            thispv = PV(pv)
+            thispv.get()
+            enums = thispv.enum_strs
+
+        ylog    = pvinfo['graph_type'].startswith('log')
+        ts, dat = arch.get_data(pv, with_current=True)
+        ylabel  = "%s\n[%s]" % (desc, pv)
+    except:
+        messages.append("data for '%s' not found" % pv)
+
+    ts2, dat2, enums2, y2label, y2log = None, None, None, None, False
+    if pv2 is not None:
+        try:
+            pvinfo2  = arch.get_pvinfo(pv2)
+            desc2    = pvinfo2['desc']
+            dtype2   = pvinfo2['type']
+            if dtype2 == 'enum':
+                thispv = PV(pv2)
+                thispv.get()
+                enums2 = thispv.enum_strs
+            y2log    = pvinfo2['graph_type'].startswith('log')
+            ts2, dat2 = arch.get_data(pv2, with_current=True)
+            y2label="%s\n[%s]" % (desc2, pv2)
+        except:
+            messages.append("data for '%s' not found" % pv2)
+
+    fig = None
+    if ts is not None:
+        fig = make_plot(ts, dat, ylabel=ylabel, ## ylog=ylog,
+                        enums=enums,
+                        ts2=ts2, dat2=dat2, y2label=y2label, # y2log=y2log,
+                        enums2=enums2,
+                        time_unit=tunit, time_val=tval)
+
+    if len(messages) > 0:
+        messages = ', '.join(messages)
+    else:
+        messages = None
+
+    opts = {'pv': pv, 'pv2': pv2, 'timevar': timevar, 'date1': date1,
+            'date2': date2, 'messages': messages, 'figure' : fig,
+            'related': related}
 
     return render_template('plot.html', **opts)
 
 @app.route('/formplot', methods=['GET', 'POST'])
 def formplot():
     return Response(" Form Plot")
-
-
-@app.route('/oplot/<pv>')
-@app.route('/oplot/<pv>/<pv2>')
-@app.route('/oplot/<pv>/<pv2>/<time_ago>')
-@app.route('/oplot/<pv>/<pv2>/<time_ago>/<date1>')
-@app.route('/oplot/<pv>/<pv2>/<time_ago>/<date1>/<date2>')
-def oplot(pv=None, pv2=None, time_ago=None, date1=None, date2=None):
-    p = PlotViewer()
-    return Response(p.do_plot(pv=pv, pv2=pv2,
-                              time_ago=time_ago,
-                              date1=date1, date2=date2))
 
 
 @app.route('/admin')
