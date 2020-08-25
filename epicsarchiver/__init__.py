99743bc6ef1684678c8a642a0a878e09b259f9e4
Matt Newville <newville@cars.uchicago.edu>
2020-08-25 12:19:43 -0500

    refactor step1

--- epicsarchiver/__init__.py
+++ epicsarchiver/__init__.py
@@ -3,21 +3,21 @@
    Matthew Newville <newville@cars.uchicago.edu>
    CARS, University of Chicago
 
-   version:      1.2.0
-   last update:  21-Oct-2010
-   copyright:    Matthew Newville, The University of Chicago, 2007
-   license:      MIT / BSD
+   version:      2.0
+   last update:  2020-June-01
+   copyright:    Matthew Newville, The University of Chicago, 2007 - 2020
+   license:      MIT 
          
 """
-__version__ = '1.2.0'
+__version__ = '2.0'
 
 import os
-from . import config
 import sys
+from . import config
 
-os.environ['PYTHON_EGG_CACHE'] = config.share_dir
-sys.path.insert(0, config.share_dir)
-import MySQLdb
+import sqlalchemy
+# sys.path.insert(0, config.share_dir)
+
     
 from .util import string_literal, clean_input, escape_string, timehash, tformat, clean_mail_message
 
@@ -29,9 +29,9 @@ from .ArchiveMaster  import ArchiveMaster
 from .Cache          import Cache, add_pvfile
 from .Archiver       import Archiver
 from .Daemon         import startstop
-from .HTMLWriter     import HTMLWriter
-from .PlotViewer     import PlotViewer
-from .WebStatus      import WebStatus
-from .WebHelp        import WebHelp
-from .WebAdmin       import WebAdmin
-from .WebInstruments import WebInstruments
+# from .HTMLWriter     import HTMLWriter
+# from .PlotViewer     import PlotViewer
+# from .WebStatus      import WebStatus
+# from .WebHelp        import WebHelp
+# from .WebAdmin       import WebAdmin
+# from .WebInstruments import WebInstruments
