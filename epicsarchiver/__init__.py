50dcefa65051c51ad2aa32008fc62017dc92e742
Matt Newville <newville@cars.uchicago.edu>
2020-08-31 17:34:44 -0500

    initial_sql exposed

--- epicsarchiver/__init__.py
+++ epicsarchiver/__init__.py
@@ -6,13 +6,13 @@
    version:      2.1
    last update:  2020-Aug-29
    copyright:    Matthew Newville, The University of Chicago, 2007 - 2020
-   license:      MIT 
-         
+   license:      MIT
+
 """
 __version__ = '2.1'
 
 from .util import get_config, tformat
 from .cache import Cache, add_pvfile
 from .archiver  import Archiver
+from .schema import initial_sql
 from .pvarch import pvarch_main
-
