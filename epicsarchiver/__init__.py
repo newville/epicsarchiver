e27f4f5b2b76350cba1ccd58873a1ba21ade4ffa
Matthew Newville <newville@cars.uchicago.edu>
2024-08-31 21:49:00 -0500

    update version

--- epicsarchiver/__init__.py
+++ epicsarchiver/__init__.py
@@ -3,13 +3,13 @@
    Matthew Newville <newville@cars.uchicago.edu>
    CARS, University of Chicago
 
-   version:      2.1
-   last update:  2020-Sept-01
-   copyright:    Matthew Newville, The University of Chicago, 2007 - 2020
+   version:      2.3
+   last update:  2024-Sept-01
+   copyright:    Matthew Newville, The University of Chicago, 2007 - 2024
    license:      MIT
 
 """
-__version__ = '2.1'
+__version__ = '2.3'
 
 from .util import get_config, tformat, hformat
 from .cache import Cache
